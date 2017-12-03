5c6a0bce7c987bcfadd63865f2041f23d6fa44d0
Danil Starovoytov <leomailreg@gmail.com>
2017-12-03 14:30:50 +0700

    Проба делать скриншоты

--- webwatcher-core/src/test/java/ru/davist/webwatcher/core/phantom/PhantomjsTest.java
+++ webwatcher-core/src/test/java/ru/davist/webwatcher/core/phantom/PhantomjsTest.java
@@ -3,16 +3,18 @@
  */
 package ru.davist.webwatcher.core.phantom;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author StarovoytovD
@@ -23,19 +25,24 @@ public class PhantomjsTest {
     private static final Logger log = LoggerFactory.getLogger(PhantomjsTest.class);
 
     @Test
-    public void test() {
+    public void test() throws IOException {
 
         File file = new File("/Users/danil/devel/bin/phantomjs-2.1.1-macosx/bin/phantomjs");
 
         System.setProperty("phantomjs.binary.path", file.getAbsolutePath());
 
         PhantomJSDriver driver = new PhantomJSDriver();
-        driver.get("http://www.dns-shop.ru/product/014cb074033a3361/45-plenka-zasitnaa-dla-smartfona-red-line-ut000006696/");
-        WebElement element = driver.findElement(By.className("price_g"));
+//        driver.get("http://www.dns-shop.ru/product/014cb074033a3361/45-plenka-zasitnaa-dla-smartfona-red-line-ut000006696/");
+        driver.get("http://store.steampowered.com/app/578080/PLAYERUNKNOWNS_BATTLEGROUNDS/");
+//        WebElement element = driver.findElement(By.className("price_g"));
+        driver.manage().window().setSize(new Dimension(1366, 768));
 
-//        driver.getScreenshotAs();
 
-        log.info("Value: " + element.getText());
+        File screenshot = driver.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot, new File("target/1.jpg"));
+
+
+//        log.info("Value: " + element.getText());
 
         driver.close();
     }
